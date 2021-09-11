499e9af7e20ac1a3dfc3e2d407ad7eac265d1684
Thada Jirajaras <thada.jir@gmail.com>
2021-09-11 07:36:22 +0700

    Add pytest for API

--- starter/main.py
+++ starter/main.py
@@ -30,23 +30,7 @@ class Features(BaseModel):
 
 # load model
 model = Model(preprocessor = process_data) 
-model.load_weights(os.path.join('model', 'model.pkl'))
-
-# provide input sample
-input_sample = {"age": "39",
-                "workclass": "State-gov",
-                "fnlgt": "77516",
-                "education": "Bachelors",
-                "education-num": "13",
-                "marital-status": "Never-married",
-                "occupation": "Adm-clerical",
-                "relationship": "Not-in-family",
-                "race": "White",
-                "sex": "Male",
-                "capital-gain": "2174",
-                "capital-loss": "0",
-                "hours-per-week": "40",
-                "native-country": "United-States"}
+model.load_weights(os.path.join('starter', 'model', 'model.pkl'))
 
 
 # Instantiate the app.
---
+++ starter/test_main.py
@@ -0,0 +1,52 @@
+from fastapi.testclient import TestClient
+
+# Import our app from main.py.
+from main import app
+
+# Instantiate the testing client with our app.
+client = TestClient(app)
+
+# Write tests using the same syntax as with the requests module.
+def test_rootaddress():
+    r = client.get("/")
+    assert r.status_code == 200
+    
+def test_get_prediction1():
+    input_sample = {"age": "39",
+                "workclass": "State-gov",
+                "fnlgt": "77516",
+                "education": "Bachelors",
+                "education-num": "13",
+                "marital-status": "Never-married",
+                "occupation": "Adm-clerical",
+                "relationship": "Not-in-family",
+                "race": "White",
+                "sex": "Male",
+                "capital-gain": "2174",
+                "capital-loss": "0",
+                "hours-per-week": "40",
+                "native-country": "United-States"}    
+    
+    r = client.post(url = "/predict", json = input_sample)
+    assert r.status_code == 200     
+    
+def test_get_prediction2():
+    # insert some 1 strange feature (native-country) and see if model still be able to get prediction 
+    input_sample = {"age": 39,
+                "workclass": "State-gov",
+                "fnlgt": "77516",
+                "education": "Bachelors",
+                "education-num": "13",
+                "marital-status": "Never-married",
+                "occupation": "Adm-clerical",
+                "relationship": "Not-in-family",
+                "race": "White",
+                "sex": "Male",
+                "capital-gain": "2174",
+                "capital-loss": "0",
+                "hours-per-week": "40",
+                "native-country": "Strange-Country"}    
+    
+    
+    r = client.post(url = "/predict", json = input_sample)
+    assert r.status_code == 200, r.status_code       
