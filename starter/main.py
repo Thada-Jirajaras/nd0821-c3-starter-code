e95ca546f51c83f4573ce185d771581bf8a41e04
Thada Jirajaras <thada.jir@gmail.com>
2021-09-12 15:56:29 +0700

    Edit dvc initial commands

--- starter/main.py
+++ starter/main.py
@@ -14,7 +14,7 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
         exit("Pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+    #os.system("rm -r .dvc .apt/usr/lib/dvc")
     
 # provide input structure
 class Features(BaseModel):
